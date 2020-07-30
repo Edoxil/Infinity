cae73b9079d446a0f22b4a293cd2b6e094c12be3
Edoxil <61228578+Edoxil@users.noreply.github.com>
2020-07-30 11:08:18 +0300

    Add healthbar UI near selected target

---
+++ Assets/Scripts/UI/TargetHealthBar.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetHealthBar : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    private Stats _stats;
+    private Slider _healthBarFiller;
+    private Camera _camera;
+
+    private void Awake()
+    {
+        Messenger<Transform>.AddListener(GameEvent.TARGET_SELECTED, Select);
+        Messenger.AddListener(GameEvent.TARGET_UNSELECTED, Unselect);
+    }
+    private void OnDestroy()
+    {
+        Messenger<Transform>.RemoveListener(GameEvent.TARGET_SELECTED, Select);
+        Messenger.RemoveListener(GameEvent.TARGET_UNSELECTED, Unselect);
+    }
+
+  
+
+   
+
+    void Start()
+    {
+        
+        _healthBarFiller = GetComponent<Slider>();
+        _camera = Camera.main;
+        gameObject.SetActive(false);
+    }
+
+    private void LateUpdate()
+    {
+        gameObject.transform.LookAt(_camera.transform);
+    }
+
+    
+
+
+    private void UpdateHealthBar()
+    {
+        _stats = _target.GetComponent<Stats>();
+        _healthBarFiller.maxValue = _stats.maxHP;
+        _healthBarFiller.value = _stats.currentHP; 
+    }
+
+
+    private void Open()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Close()
+    {
+        gameObject.SetActive(false);
+    }
+    private void Select(Transform target)
+    {
+        if(_target == target)
+        {
+            UpdateHealthBar();
+            Open();
+        }
+        else
+        {
+            Unselect();
+        }
+    }
+    private void Unselect()
+    {
+        Close();
+    }
+}
+       
+        
+
+        
+        
+        
