4dff1b4f2f8d5d69d985f1e87e821b64e63928ab
Edoxil <61228578+Edoxil@users.noreply.github.com>
2020-08-04 10:09:51 +0300

    Revert "Starts combat system"
    This reverts commit 23e66de97948cefb06bbc64bc7b7f9ad8f37d3e6.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,35 +5,18 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
-[RequireComponent(typeof(Stats))]
 public class Enemy : MonoBehaviour
 {
     public Vector3 start;
     public Vector3 end;
     private NavMeshAgent _agent;
-    private Stats _stats;
-    private State _state = State.Peace;
-
-
-    private float _attackDelay = 1.5f;
-
-
     bool flag = true;
-
-    private enum State
-    {
-       Battle,
-       Peace
-    }
-
-
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         start = transform.position;
         end = transform.position;
         end.x += 15f;
-        _stats = GetComponent<Stats>();
     }
 
     
@@ -53,35 +36,13 @@ public class Enemy : MonoBehaviour
             }
 
         }
-
-
-        if(_state == State.Battle)
-        {
-            TakeDamage(1);
-        }
     }
-
-
-
-    private void TakeDamage(int damage)
-    {
-        _stats.currentHP -= damage;
-    }
-
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            _state = State.Battle;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            _state = State.Peace;
+            Debug.Log("Can attack");
         }
     }
 }
