cfca2486dc3951ffb0288b504b51a2c288e075d7
Edoxil <61228578+Edoxil@users.noreply.github.com>
2020-08-12 10:17:34 +0300

    Add combat floatingtext near target

--- Assets/Scripts/Combat/MeleCombat.cs
+++ Assets/Scripts/Combat/MeleCombat.cs
@@ -1,24 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class MeleCombat : MonoBehaviour
 {
     private CharacterStats _playerStats;
     private CharacterStats _targetStats;
+    private Transform _thisTransform;
+    private Transform _targetTransform;
     [SerializeField] private Animator _animator;
+    [SerializeField] private GameObject _floatingTextPrefab;
+    private TextMesh _textMesh;
     private float _attackCooldown = 0f;
-    
+   
     private void Start()
     {
         _playerStats = GetComponentInParent<CharacterStats>();
-        
-        
+        _textMesh = _floatingTextPrefab.GetComponent<TextMesh>();
+        _thisTransform = GetComponent<Transform>();
     }
 
 
 
     void Update()
     {
-
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            _animator.SetFloat("AttackSpeed", 2f);
+            
+        }
         _attackCooldown -= Time.deltaTime;
 
         if (_targetStats != null)
@@ -37,6 +46,8 @@ public class MeleCombat : MonoBehaviour
             dmgAmaunt -= _targetStats.defense.GetValue();
             int hp = _targetStats.currentHP.GetValue() - dmgAmaunt;
 
+            ShowFloatingText(dmgAmaunt.ToString(),Color.green);
+
             _targetStats.currentHP.SetValue(hp);
             _attackCooldown = 1 / _playerStats.attackspeed.GetValue();
             Debug.Log(_targetStats.name + " take dmg " + dmgAmaunt);
@@ -44,13 +55,19 @@ public class MeleCombat : MonoBehaviour
 
     }
 
-
+    private void ShowFloatingText(string text, Color color)
+    {
+        _textMesh.color = color;
+        _textMesh.text = text;
+        
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy"))
         {
             _targetStats = other.GetComponent<CharacterStats>();
+            _targetTransform = other.transform;
         }
     }
     private void OnTriggerExit(Collider other)
@@ -58,6 +75,7 @@ public class MeleCombat : MonoBehaviour
         if (other.CompareTag("Enemy"))
         {
             _targetStats = null;
+            _targetTransform = null;
         }
     }
 }
---
+++ Assets/Scripts/UI/FloatingText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class FloatingText : MonoBehaviour
+{
+    private float _destroyTime = 2f;
+    private Vector3 _offset = new Vector3(0f, 2.5f, 0);
+    private Vector3 _randomaizeIntensity = new Vector3(0.5f, 0, 0);
+    void Start()
+    {
+        Destroy(this.gameObject, _destroyTime);
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(0, 180, 0);
+        transform.localPosition += _offset;
+        transform.localPosition += new Vector3(Random.Range(-_randomaizeIntensity.x, _randomaizeIntensity.x),
+            Random.Range(-_randomaizeIntensity.y, _randomaizeIntensity.y),
+            Random.Range(-_randomaizeIntensity.z, _randomaizeIntensity.z));
+
+
+    }
+
+
+}
